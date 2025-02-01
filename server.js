4c503b5144f08957443c90d1a40e6ca786b6ac77
Vishwanath <vishwanatharuchunan@gmail.com>
2025-02-01 19:55:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
